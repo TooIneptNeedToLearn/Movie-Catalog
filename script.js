84ce31a9e5b9d06a313e62a846002f877743de3b
TooIneptNeedToLearn <moltencore23@gmail.com>
2023-12-23 17:00:45 +0800

    added the search functionality, tweaking the UI

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
+const baseurl = "https://api.themoviedb.org/3";
 const api =
   "https://api.themoviedb.org/3/movie/upcoming?api_key=d6d60f5f0e645febd6593c7c8b0e2c38&language=en-US&page=1";
 const image_url = "https://image.tmdb.org/t/p/w500";
+const search_url =
+  baseurl + "/search/movie?api_key=d6d60f5f0e645febd6593c7c8b0e2c38";
 
 getMovies(api);
 function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
       showMovies(data.results);
     });
 }
@@ -41,3 +45,12 @@ function getColor(vote) {
     return "red";
   }
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchform = search.value;
+  if (searchform) {
+    getMovies(search_url + "&query=" + searchform);
+  }
+});
