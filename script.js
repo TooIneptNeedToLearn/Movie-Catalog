9387b57385f94d960e4d484dfa936285956912cd
TooIneptNeedToLearn <moltencore23@gmail.com>
2023-12-29 21:12:50 +0800

    updated the css finalized the look

--- script.js
+++ script.js
@@ -25,16 +25,18 @@ const showMovies = async () => {
     `https://api.themoviedb.org/3/movie/upcoming?api_key=d6d60f5f0e645febd6593c7c8b0e2c38&language=en-US&page=1`
   );
   const data = await response.json();
-  container.innerHTML = data.results.map(
-    ({ id, backdrop_path, original_title, vote_average }) => `
-  <button class="poster-banner" ondblclick="moviedetails(${id})">
+  container.innerHTML = data.results
+    .map(
+      ({ id, backdrop_path, original_title, vote_average }) => `
+  <div class="poster-banner" ondblclick="moviedetails(${id})">
       <img src="${image_url + backdrop_path}" alt ="${original_title}">
       <div class = "movie-info">
           <h3>${original_title}</h3>
           <span class="${getColor(vote_average)}">${vote_average}</span>
       </div>
-  </button>`
-  );
+  </div>`
+    )
+    .join("");
 };
 
 const searchmovies = async (name) => {
@@ -42,16 +44,18 @@ const searchmovies = async (name) => {
     `https://api.themoviedb.org/3/search/movie?api_key=d6d60f5f0e645febd6593c7c8b0e2c38&query=${name}`
   );
   const data = await response.json();
-  container.innerHTML = data.results.map(
-    ({ id, backdrop_path, original_title, vote_average }) => `
-    <button class="poster-banner" ondblclick="moviedetails(${id})">
+  container.innerHTML = data.results
+    .map(
+      ({ id, backdrop_path, original_title, vote_average }) => `
+    <div class="poster-banner" ondblclick="moviedetails(${id})">
         <img src="${image_url + backdrop_path}" alt ="${original_title}">
-        <div class"movie-info">
+        <div class="movie-info">
             <h3>${original_title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
         </div>
-    </button>`
-  );
+    </div>`
+    )
+    .join("");
 };
 
 const moviedetails = async (id) => {
@@ -67,30 +71,29 @@ const moviedetails = async (id) => {
   const similarMoviesHTML = similarmov.results
     .map(
       ({ id, backdrop_path, original_title, vote_average }) => `
-    <button class="poster-banner" onclick="moviedetails(${id})">
+    <div class="poster-banner" onclick="moviedetails(${id})">
         <img src="${image_url + backdrop_path}" alt="${original_title}">
         <div class="movie-info">
             <h3>${original_title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
         </div>
-    </button>`
+    </div>`
     )
     .join("");
 
   container.innerHTML = `
   <div class="con">
-    <div class="image-container">
-      <img src="${image_url + data.backdrop_path}" alt="Movie Poster"></img>
-    </div>
     <div class="details-container">
+        <img src="${image_url + data.backdrop_path}" alt="Movie Poster"></img>
         <h1><span>Title:</span> ${data.original_title}</h1>
         <h1><span>Genres: </span>${data.genres.map((data) => data.name)}</h1>
         <p>${data.overview}</p>
+        <div class="simmov">
+          <h1><span>Similar Movies:</span></h1>
+          <div class="similar-movies">${similarMoviesHTML}</div>
+        </div>
     </div>
-  </div>
-  <div class="simmov">
-  <h1><span>Similar Movies:</span></h1>
-  <div class="similar-movies">${similarMoviesHTML}</div>
+
   </div>`;
 };
 
